468a65ae60dd11185f99573297f00a6b43ff9531
ImAndromeda <80559738+ImAndromeda@users.noreply.github.com>
2023-04-04 23:38:49 -0400

    Added a rate-limiting mechanism
    Added a rate-limiting mechanism using a BPF_MAP_TYPE_PERCPU_ARRAY map

--- fivem_xdp.c
+++ fivem_xdp.c
@@ -3,14 +3,25 @@
 #include <linux/ip.h>
 #include <linux/in.h>
 #include <linux/udp.h>
+#include <bpf/bpf_helpers.h>
 
 #define FIVEM_SERVER_IP     192.168.1.1      // Replace with the IP address of your FiveM server
 #define FIVEM_SERVER_PORT   30120           // Replace with the port number of your FiveM server
+#define RATE_LIMIT          100             // Maximum number of packets allowed per second
+
+struct bpf_map_def SEC("maps") rate_limit_map = {
+    .type = BPF_MAP_TYPE_PERCPU_ARRAY,
+    .key_size = sizeof(__u32),
+    .value_size = sizeof(__u64),
+    .max_entries = 1,
+};
 
 SEC("xdp_program")
 int fivem_xdp(struct xdp_md *ctx) {
     void *data_end = (void *)(long)ctx->data_end;
     void *data = (void *)(long)ctx->data;
+    __u32 rate_limit_key = 0;
+    __u64 *rate_limit_value;
 
     // Parse Ethernet header
     struct ethhdr *eth = data;
@@ -39,6 +50,24 @@ int fivem_xdp(struct xdp_md *ctx) {
     if (ip->daddr != htonl(FIVEM_SERVER_IP) || udp->dest != htons(FIVEM_SERVER_PORT)) {
         return XDP_DROP;
     }
+
+    // Check the rate limit map
+    rate_limit_value = bpf_map_lookup_elem(&rate_limit_map, &rate_limit_key);
+    if (!rate_limit_value) {
+        return XDP_ABORTED;
+    }
+
+    // Get the current timestamp
+    __u64 now = bpf_ktime_get_ns();
+    __u64 last = *rate_limit_value;
+
+    // Rate limiting logic
+    if (now - last < (1000000000 / RATE_LIMIT)) {
+        return XDP_DROP;
+    }
+
+    // Update the rate limit map with the current timestamp
+    bpf_map_update_elem(&rate_limit_map, &rate_limit_key, &now, BPF_ANY);
 
     return XDP_PASS;
 }
